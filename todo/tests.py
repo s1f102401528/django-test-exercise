24c5f0111200bc84207826f09bd1c3ae4787d010
1F10240152 <s1f102401528@iniad.org>
2025-07-03 11:28:21 +0900

    コードを整形２

--- todo/tests.py
+++ todo/tests.py
@@ -84,7 +84,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
-    
+
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()

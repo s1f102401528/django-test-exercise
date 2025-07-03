edc358a682348d7025fd31b3ccf9116abb69852f
1F10240152 <s1f102401528@iniad.org>
2025-07-03 14:45:18 +0900

    詳細ページのテストを修正２

--- todo/tests.py
+++ todo/tests.py
@@ -102,7 +102,7 @@ class TodoViewTestCase(TestCase):
         task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task.save()
         client = Client()
-        response = client.get('/{}/.format(task.pk)')
+        response = client.get('/{}/'.format(task.pk))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/detail.html')
@@ -111,5 +111,5 @@ class TodoViewTestCase(TestCase):
     def test_detail_get_fail(self):
         client = Client()
         response = client.get('/1/')
-
+        
         self.assertEqual(response.status_code, 404)

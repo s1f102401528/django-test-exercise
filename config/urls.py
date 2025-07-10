76233138c92632ffb4b2ca6f3ea9457692a2cb9e
1f10240009 <s1f102400099@iniad.org>
2025-07-10 18:54:33 +0900

    reset

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
-    path('<int:task_id>/update', todo_views.update, name='update'),
-
 ]
